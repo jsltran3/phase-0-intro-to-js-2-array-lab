6b5edfa1a04794e93eb0a7c186d6b0a462fbb66a
Joseph <jsltran@gmail.com>
2021-12-23 18:41:20 -0800

    commit tree 8abd6021902e31ffe8e2d0d15b0b66ee743eee78

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Write your solution here!
+const cats =  ['Milo', "Otis", "Garfield"]; 
+
+function destructivelyAppendCat() {
+    cats.push('Ralph');
+}
+
+function destructivelyPrependCat() {
+    cats.unshift('Bob');
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop(0);
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift(0);
+}
+
+function appendCat() {
+    let copyCat = [...cats, 'Broom'];
+    return copyCat;
+}
+
+function prependCat() {
+    let copyCat = ['Arnold', ...cats];
+    return copyCat;
+}
+
+function removeLastCat() {
+    let oneCatMissing = cats.slice(0, -1);
+    return oneCatMissing; 
+}
+
+// function removeFirstCat() {
+//     let oneCatMissing = cats.slice(0, cats.length - 1); 
+//     return oneCatMissing; 
+
+
+
+function removeFirstCat() {
+    let oneCatMissing = cats.slice(1);
+    return oneCatMissing; 
+}
